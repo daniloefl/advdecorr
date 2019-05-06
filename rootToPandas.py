a031fb686bf88e0a8c749e91dcaf0e37eb1816a9
Danilo Enoque Ferreira De Lima <danilo.enoque.ferreira.de.lima@cern.ch>
2019-05-06 16:34:02 +0200

    Using the toy model uncertainty variation.

--- rootToPandas.py
+++ rootToPandas.py
@@ -10,11 +10,11 @@ from __future__ import print_function
 
 # use this to read the ROOT file and save it in a .h5 file to use with Pandas (faster)
 def transformROOTToPandas(treeNameList = ["Nominal",
-                                          #'slope',
+                                          'slope',
                                           #'JET_JER_SINGLE_NP__1up',
                                           #'JET_SR1_JET_GroupedNP_1__1up',# 'JET_SR1_JET_GroupedNP_2__1up', 'JET_SR1_JET_GroupedNP_3__1up',
                                           #'TAUS_TRUEHADTAU_SME_TES_DETECTOR__1up', 'TAUS_TRUEHADTAU_SME_TES_INSITU__1up',
-                                          'TAUS_TRUEHADTAU_SME_TES_MODEL__1up'
+                                          #'TAUS_TRUEHADTAU_SME_TES_MODEL__1up'
                                          ],
                           signalName = "vbf_hh_l1cvv2cv1.root",
                           bkgName = "ttbar_PwPy8EG.root"):
