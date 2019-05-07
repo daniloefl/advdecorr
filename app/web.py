026e70ac59784425605a47078cc62570b7ed3400
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-05-07 12:37:33 +0200

    Bug fix.

--- app/web.py
+++ app/web.py
@@ -5,8 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def web()
+def web():
   return 'Test'
 
-if __name == '__main__':
+if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
