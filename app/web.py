10c228c48891aa03c3d7b0ed4345037edf4d5029
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-05-07 22:05:10 +0200

    Created simple interface for testing data exchange between Flask and a production server-side inference module.

--- app/web.py
+++ app/web.py
@@ -1,12 +1,25 @@
 # app/web.py
 
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def web():
-  return 'Test'
+  return flask.render_template('index.html')
+
+@app.route('/result', methods = ['POST'])
+def result():
+  data = flask.request.form['data']
+  result = {}
+  i = 0
+  for line in data.split('\n'):
+    content = line.split()
+    result[i] = {'pvalue' : 0}
+    for k in range(0, len(content)):
+      result[i]['input_%d' %k] = content[k]
+  return flask.render_template('result.html', result = result)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
