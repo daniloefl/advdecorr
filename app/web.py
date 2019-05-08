d368f75395b05adb8bd43628a442ff3db59efaf9
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-05-09 00:27:50 +0200

    Updated Dockerfile to add requests module and added call to server.

--- app/web.py
+++ app/web.py
@@ -6,6 +6,8 @@ from flask_nav import Nav
 from flask_nav.elements import Navbar, View
 from flask_restful import Resource, Api
 
+import requests
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 nav = Nav()
@@ -31,9 +33,10 @@ def result():
   N = len(A)
   result = {}
   for i in range(0, N):
-    result[i] = {'pvalue' : 0}
+    result[i] = {}
+    result[i]['pvalue'] = requests.put('http://localhost:5001/classify', data={'i': i, 'A': A[i], 'B': B[i]})
     result[i]['A'] = A[i]
-    result[i]['B'] = A[i]
+    result[i]['B'] = B[i]
   return render_template('result.html', result = result)
 
 if __name__ == '__main__':
