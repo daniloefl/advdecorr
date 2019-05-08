7b6c169c4d83cce153aed1e38eed27dc03e7437f
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-05-09 00:23:27 +0200

    Created separated Dockerfiles for client and server.

--- app/web.py
+++ app/web.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
