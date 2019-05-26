f8135d82ae7675a7e9013e0560f9250760f617a6
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-05-26 19:05:50 +0200

    Corrected script to get correct network file after test training.

--- training/test_training.sh
+++ training/test_training.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ./generateToys.py
-./keras_gan.py --input-file input_toys.h5 --prefix gan --mode train --iterations 51
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode train --iterations 51
+./keras_gan.py --input-file input_toys.h5 --prefix gan --mode train --iterations 101
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode train --iterations 101
 
-./keras_gan.py --input-file input_toys.h5 --prefix gan --mode plot_loss --load-trained 51
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode plot_loss --load-trained 51
+./keras_gan.py --input-file input_toys.h5 --prefix gan --mode plot_loss --load-trained 100
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode plot_loss --load-trained 100
 
-./keras_gan.py --input-file input_toys.h5 --prefix gan --mode plot_output --load-trained 51
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode plot_output --load-trained 51
+./keras_gan.py --input-file input_toys.h5 --prefix gan --mode plot_output --load-trained 100
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode plot_output --load-trained 100
 
