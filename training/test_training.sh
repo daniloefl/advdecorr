8f7d99b2fe26c4d9b2931996b3e0b42e68a487c6
Danilo Enoque Ferreira De Lima <danilo.enoque.ferreira.de.lima@cern.ch>
2019-05-27 17:23:57 +0200

    Adapted test after merge.

--- training/test_training.sh
+++ training/test_training.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ./generateToys.py
-./keras_gan.py --input-file input_toys.h5 --prefix gan --mode train --iterations 101
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode train --iterations 101
+./keras_gan.py --input-file input_toys.h5 --prefix gan --train --iterations 101
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --train --iterations 101
 
-./keras_gan.py --input-file input_toys.h5 --prefix gan --mode plot_loss --load-trained 100
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode plot_loss --load-trained 100
+./keras_gan.py --input-file input_toys.h5 --prefix gan --plot-loss --load-trained 100
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --plot-loss --load-trained 100
 
-./keras_gan.py --input-file input_toys.h5 --prefix gan --mode plot_output --load-trained 100
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --mode plot_output --load-trained 100
+./keras_gan.py --input-file input_toys.h5 --prefix gan --plot-output --load-trained 100
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --plot-output --load-trained 100
 
