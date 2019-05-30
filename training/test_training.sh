45b37cf36e675a6f762628e42efd0d4b9285cafb
Danilo Enoque Ferreira de Lima <daniloefl@gmail.com>
2019-05-30 13:41:11 +0200

    Set learning rates from command line.

--- training/test_training.sh
+++ training/test_training.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ./generateToys.py
-./keras_gan.py --input-file input_toys.h5 --prefix gan --train --iterations 101
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --train --iterations 101
+./keras_gan.py --input-file input_toys.h5 --prefix gan --train --iterations 101 --lr 1e-4
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --train --iterations 101 --lr 1e-4
 
 ./keras_gan.py --input-file input_toys.h5 --prefix gan --plot-loss --load-trained 100
 ./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --plot-loss --load-trained 100
