da57d542e5002777453f117c5b4007d1289286c8
Danilo Enoque Ferreira De Lima <danilo.enoque.ferreira.de.lima@cern.ch>
2019-06-04 13:19:39 +0200

    Changed training test script for Travis.

--- training/test_training.sh
+++ training/test_training.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ./generateToys.py
-./keras_gan.py --input-file input_toys.h5 --prefix gan --train --iterations 51 --lr 1e-4
-./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --train --iterations 51 --lr 1e-4
+./keras_gan.py --input-file input_toys.h5 --prefix gan --train --load-trained 0 --iterations 51 --lr 1e-4
+./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --train --load-trained 0 --iterations 51 --lr 1e-4
 
 ./keras_gan.py --input-file input_toys.h5 --prefix gan --plot-loss --load-trained 50
 ./keras_gan.py --input-file input_toys.h5 --prefix ganna --no-adv --plot-loss --load-trained 50
